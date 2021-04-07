ec3c9f7a08f298e7a33f913a41edb2cb3b7f5c54
Armin Dzibric <armindzibric6@gmail.com>
2021-04-07 14:13:55 +0200

    Fix mark-as-done bug

--- js/script.js
+++ js/script.js
@@ -54,6 +54,20 @@ function displayData(duty) { // inject duty name and functions in HTML
     document.getElementById("list").innerHTML += '<h3 class="dutyName" property="' + duty.id + '">' + duty.name + "</h3>";
     document.getElementById("list").innerHTML += '<img class="isDoneBtn" property="' + duty.id + '" src="/img/icon/false.png" onclick="markProcess(' + duty.id + ');"></img>';
     document.getElementById("list").innerHTML += '<img class="removeBtn" property="' + duty.id + '" src="/img/icon/garbageIcon.png" onclick="deleteContent(' + duty.id + ');"></img>';
+
+    if (document.querySelector('[property="' + duty.id + '"]') == duty.id) {
+
+        /*
+        while (document.querySelector('[property="' + duty.id + '"]').getAttribute("class") != "isDoneBtn") {
+
+        }*/
+
+
+    }
+
+    console.log();
+
+
 };
 
 
@@ -63,15 +77,18 @@ function markProcess(idOfDuty) {
 
     if (!duty.isDone) { // duty.isDone == 0
         duty.do();
+        // find button element based on it's id in property & change img
+        document.querySelector('[class="isDoneBtn"][property="' + duty.id + '"]').src = "/img/icon/true.png"
 
-        document.getElementsByClassName("isDoneBtn")[idOfDuty].src = "/img/icon/true.png";
 
     } else {
         duty.undo();
-        document.getElementsByClassName("isDoneBtn")[idOfDuty].src = "/img/icon/false.png";
+        // find button element based on it's id in property & change img
+        document.querySelector('[class="isDoneBtn"][property="' + duty.id + '"]').src = "/img/icon/false.png";
+
 
     }
-
+    // apply changes to list
     updateIsDoneInListOfDuties(duty, duty.isDone);
 
 };
